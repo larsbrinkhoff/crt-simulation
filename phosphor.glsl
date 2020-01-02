1a5442222fa3f7d69eb94c2e7f3c8ce9c1e89f21
Lars Brinkhoff <lars@nocrew.org>
2020-01-02 12:10:00 +0100

    P7 yellow is excited by blue light, rather than the electron beam.

--- phosphor.glsl
+++ phosphor.glsl
@@ -4,9 +4,13 @@ precision mediump float;
 
 uniform sampler2D phosphor;
 
+/* This models P7 phosphor.  The yellow layer is excited by light from
+   the blue layer. */
+
 void main(void)
 {
   float r = 0.992 * texture2D(phosphor, gl_FragCoord.xy / 1024.0).r;
   float g = 0.83 * texture2D(phosphor, gl_FragCoord.xy / 1024.0).g;
+  r += 0.1 * g;
   gl_FragColor = vec4(r,g,0.0,1.0);
 }
--- point.glsl
+++ point.glsl
@@ -16,10 +16,14 @@ float gaussian(void)
   return intensity*focus*exp(-focus*(x2+y2));
 }
 
+/* This models P7 phosphor.  The incident electron beam only excites
+   the blue layer.  The yellow layer is mostly insensitive to
+   electrons. */
+
 void main(void)
 {
   int i;
   vec2 rg = texture2D(phosphor, gl_FragCoord.xy / 1024.0).rg;
-  rg += gaussian();
+  rg.g += gaussian();
   gl_FragColor = vec4(rg,0.0,1.0);
 }

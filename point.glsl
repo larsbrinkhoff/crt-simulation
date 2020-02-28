dd9a65100e72fbbc89b9086c3f6966269ad6a0d1
Lars Brinkhoff <lars@nocrew.org>
2020-02-28 15:05:39 +0100

    Generalized Gaussian.

--- point.glsl
+++ point.glsl
@@ -13,7 +13,8 @@ float gaussian(void)
   float y2 = gl_FragCoord.y - xy.y;
   x2 *= x2;
   y2 *= y2;
-  return intensity*focus*exp(-focus*(x2+y2));
+  float r2 = x2+y2;
+  return intensity*focus*exp(-focus*r2*r2);
 }
 
 /* This models P7 phosphor.  The incident electron beam only excites
